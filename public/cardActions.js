7d330fcfae36b61a3358b55aceca50b0bb0acbdb
petercode1 <pr.gail@comcast.net>
2015-01-11 18:05:07 -0700

    Created editable areas, began responsive fixes

--- public/cardActions.js
+++ public/cardActions.js
@@ -1,37 +1,53 @@
 $(document).on('ready', function() {
 
 
-var edit = function () {
+	var edit = function () {
+		$('.editable').hide();
+		$('.write').show();
 
-};
+	};
+
+	var save = function () {
+
+		$('.editable').show();
+		$('.write').hide();
+	};
 
 
-var showPopup = function (button, id) {
+	var showPopup = function (button, id) {
 
+		var allContent = button.closest('.content-container');
 
+		switch (id) {
 
-	var allContent = button.closest('.content-container');
+			case 'see-bio':
+				allContent.find('.extra-bio').slideToggle(500);
+				break;
+			case 'see-skill':
+				allContent.find('.extra-skills').slideToggle(500);
+				break;
+			case 'see-info':
+				allContent.find('.extra-info').slideToggle(500);
+				break;
+		}
+	};
 
-	switch (id) {
-
-		case 'see-bio':
-			allContent.find('.extra-bio').slideToggle(500);
-			break;
-		case 'see-skill':
-			allContent.find('.extra-skills').slideToggle(500);
-			break;
-		case 'see-info':
-			allContent.find('.extra-info').slideToggle(500);
-			break;
-	}
-};
 
 
 	$('.btn-list button').on('click', function() {
-		console.log('Clicked');
 		var nthis = $(this);
 
 		showPopup(nthis, nthis.attr('id'));
+	});
+
+
+
+	$('.btn-edit').on('click', function() {
+		edit();
+	});
+
+	$('.btn-save').on('click', function() {
+		save();
 	});
 
 }); // --> END JQUERY
